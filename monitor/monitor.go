a0abbd1071f4baa7fc29496659a7faa18a48b733
ThisIsntTheWay <msblauwal6@gmail.com>
2024-11-20 13:42:24 +0000

    Monitor numbers [WIP]

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -14,7 +14,7 @@ import (
 
 type ActionableNumber struct {
 	NumberName string
-	MustAct    bool
+	MustActNow bool
 }
 
 // Monitor HX areas
@@ -25,15 +25,21 @@ func MonitorHxAreas() {
 	}
 
 	var actionableNumbers []ActionableNumber
-	for _, v := range hxAreas {
-		mustAct := v.NextAction.Unix() < time.Now().Unix()
-		slog.Info("MONITOR", "area", v.Area, "nextAction", v.NextAction, "numberName", v.NumberName, "mustAct", mustAct)
+	for _, hxArea := range hxAreas {
+		mustActNow := time.Now().After(hxArea.NextAction)
+		slog.Info(
+			"MONITOR",
+			"area", hxArea.Name,
+			"nextAction", hxArea.NextAction,
+			"numberName", hxArea.NumberName,
+			"mustActNow", mustActNow,
+		)
 
-		if mustAct {
+		if mustActNow {
 			// Check if this number is not already queued for action
 			isQueued := false
 			for _, a := range actionableNumbers {
-				if a.NumberName == v.NumberName && a.MustAct {
+				if a.NumberName == hxArea.NumberName && a.MustActNow {
 					isQueued = true
 					slog.Debug("MONITOR", "numberName", a.NumberName, "skipped", true, "reason", "Already queued")
 					break
@@ -44,34 +50,43 @@ func MonitorHxAreas() {
 				continue
 			}
 
-			number, err := db.GetDocument[models.Number]("numbers", bson.D{{"name", v.NumberName}})
+			number, err := db.GetDocument[models.Number]("numbers", bson.M{"name": hxArea.NumberName})
 			if err != nil {
 				slog.Error("MONITOR",
-					"message", fmt.Sprintf("Could not enumerate number '%s'", v.NumberName),
+					"message", fmt.Sprintf("Could not enumerate number '%s'", hxArea.NumberName),
 					"error", err.Error(),
 				)
+				continue
 			}
 
-			actionableNumbers = append(actionableNumbers, ActionableNumber{v.NumberName, mustAct})
-			slog.Info("MONITOR", "numberName", v.NumberName, "number", number[0].Number)
+			actionableNumbers = append(actionableNumbers, ActionableNumber{hxArea.NumberName, mustActNow})
 
-			// ToDo: Act
-			// CallAndTranscribeNumber(...)
+			slog.Info("MONITOR", "numberName", hxArea.NumberName, "number", number[0].Number, "action", "call")
+			InitCallAndTranscription(number[0].Number)
+
+			// Set last action
+			db.UpdateDocument(
+				"hx_areas",
+				bson.M{"_id": hxArea.ID},
+				bson.D{{"$set",
+					bson.D{{"last_action", time.Now()}},
+				}},
+			)
 		}
 	}
 }
 
 // Call a number and start transcription
-func InitCallAndTranscription(number string, numberName string) error {
-	call, err := caller.Call(number)
+func InitCallAndTranscription(number string) caller.CallResponse {
+	call, err := caller.Call(number, true)
 	if err != nil {
 		slog.Error("MONITOR",
-			"message", fmt.Sprintf("Failure calling number '%s' (%s)", number, numberName),
+			"message", fmt.Sprintf("Failure calling number '%s'", number),
 			"error", err.Error(),
 		)
+
+		logger.LogErrorFatal("MONITOR", err.Error())
 	}
 
-	// ToDo: Start transcript
-
-	return nil
+	return call
 }
