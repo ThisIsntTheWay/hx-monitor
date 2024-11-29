c15718c6e6bc4738c360897367ddaa11380badc0
ThisIsntTheWay <msblauwal6@gmail.com>
2024-11-29 12:48:18 +0000

    Add comment

--- logger/logger.go
+++ logger/logger.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 )
 
+// Logs a fatal error and panics
 func LogErrorFatal(module string, message string) error {
 	slog.Error(module, "fatal", message)
 	panic(fmt.Sprintf("%s: %s", module, message))
