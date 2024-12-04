6f091f7299c42e48b33948df8c12e8607d1a7353
ThisIsntTheWay <msblauwal6@gmail.com>
2024-12-04 17:45:52 +0000

    Set listen port using LISTEN_PORT #3

--- api-backend/main.go
+++ api-backend/main.go
@@ -4,18 +4,25 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/thisisnttheway/hx-checker/configuration"
 	"github.com/thisisnttheway/hx-checker/logger"
 )
 
-const port string = "8080"
+const defaultPort string = "8080"
 
 func main() {
 	configuration.SetUpMongoConfig()
 
-	slog.Info("MAIN", "action", "startServer", "port", port, "apiBase", apiBase)
-	err := http.ListenAndServe(":"+port, muxRouter)
+	listenPort, exists := os.LookupEnv("LISTEN_PORT")
+	if !exists {
+		slog.Warn("MAIN", "message", "LISTEN_PORT is unset, using default", "default", defaultPort)
+		listenPort = defaultPort
+	}
+
+	slog.Info("MAIN", "action", "startServer", "port", listenPort, "apiBase", apiBase)
+	err := http.ListenAndServe(":"+listenPort, muxRouter)
 	if err != nil {
 		logger.LogErrorFatal("MAIN", fmt.Sprintf("Webserver was unable to start: %v", err))
 	}
