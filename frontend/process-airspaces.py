3bdadb220bc8857a5c4ab7870f764aa7e8a073ac
ThisIsntTheWay <msblauwal6@gmail.com>
2024-12-05 14:53:11 +0100

    Add --force arg, more verbosity

--- frontend/process-airspaces.py
+++ frontend/process-airspaces.py
@@ -1,12 +1,13 @@
 # Acquire latest airspace GeoJSON but only retain important info
 import json
 import urllib.request
+import argparse
 from datetime import datetime, timedelta
 from pathlib import Path
 
 airspaces_json_url = "https://airspace.shv-fsvl.ch/api/v1/geojson/airspaces"
-airspaces_json = Path("./public/shv_airspaces.json")
-airspaces_json_processed = Path("./public/shv_airspaces_processed.json")
+airspaces_json = Path("./files/shv_airspaces.json")
+airspaces_json_processed = Path("./files/shv_airspaces_processed.json")
 relevant_areas = ["meiringen"]
 
 def download_airspaces_json(force=False):
@@ -27,19 +28,30 @@ def process_airspaces_json():
     features = data['features']
 
     # Filter entries where 'properties.Name' matches any of the candidates
-    filtered_features = [
-        f for f in features
-        if f.get('properties', {}).get('HX', False) 
-        and any(c in f.get('properties', {}).get('Name', '').lower() for c in relevant_areas)
-    ]
+    filtered_features = []
+    for f in features:
+        properties = f.get('properties', {})
+        if not properties.get('HX', False):
+            continue
+        for c in relevant_areas:
+            name = properties.get('Name', '').lower()
+            if c in name:
+                print(f"> Match for '{c}': {name}")
+                break
+        else:
+            continue
+        
+        filtered_features.append(f)
             
     with open(airspaces_json_processed, 'w', encoding='utf-8') as outfile:
         json.dump(filtered_features, outfile, indent=2)
 
 # ------------------
-def main():
+def main(force=False):    
     print(f"Verifying '{airspaces_json}'...")
-    if airspaces_json.is_file():
+    print(f"Relevant areas: {relevant_areas}")
+    
+    if airspaces_json.is_file() and not force:
         last_modified = datetime.fromtimestamp(airspaces_json.stat().st_mtime)
         time_diff = datetime.now() - last_modified
         
@@ -49,9 +61,14 @@ def main():
             print(f"JSON still within acceptable timedelta: {timedelta(weeks=1) - time_diff}")
             return
     else:
-        download_airspaces_json()
+        download_airspaces_json(True)
 
     process_airspaces_json()
+    print("Done processing")
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Force download and processing JSON")
+    parser.add_argument("--force", action="store_true", help="Force download and processing of JSON")
+    args = parser.parse_args()
+    
+    main(args.force)
