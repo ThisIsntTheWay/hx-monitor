40ad95b6db2ae0949078d5e528b744c2dc665148
ThisIsntTheWay <msblauwal6@gmail.com>
2024-12-17 12:38:43 +0000

    Move getTranscriptAggregationPipeline(), handleTranscripts()

--- api-backend/routes.go
+++ api-backend/routes.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"time"
 
 	"github.com/gorilla/mux"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type ResponseOk struct {
@@ -37,73 +33,4 @@ func init() {
 	// Transcripts
 	muxRouter.HandleFunc(apiBase+"transcripts/{name:[^/]+}/latest", getTranscriptsLatest).Methods("GET")
 	muxRouter.HandleFunc(apiBase+"transcripts/{name:[^/]+}", getTranscripts).Methods("GET")
-}
-
-// Get a mongo.Pipeline for transcript lookups. If specified, inject will be placed inbetween $unwind and $project.
-func getTranscriptAggregationPipeline(areaName string, inject []bson.D) mongo.Pipeline {
-	match := bson.D{{"$match", bson.M{"name": areaName}}}
-	lookup := bson.D{{"$lookup", bson.D{
-		{"from", "transcripts"},
-		{"localField", "_id"},
-		{"foreignField", "hx_area_id"},
-		{"as", "related_transcripts"},
-	}}}
-	unwind := bson.D{{"$unwind", "$related_transcripts"}}
-	project := bson.D{{"$project", bson.M{
-		"transcript": "$related_transcripts.transcript",
-		"date":       "$related_transcripts.date",
-	}}}
-
-	p := mongo.Pipeline{match, lookup, unwind}
-	if len(inject) > 0 {
-		for _, in := range inject {
-			p = append(p, in)
-		}
-	}
-	p = append(p, project)
-
-	return p
-}
-
-// Handles a transcript aggregation result
-func handleTranscripts(t []transcriptAggregation, e error, a string, w http.ResponseWriter) interface{} {
-	var errType string
-	if e == nil {
-		if len(t) == 0 {
-			errType = "notfound"
-			e = fmt.Errorf("Found no transcripts for area '%s'", a)
-		}
-	}
-
-	var s interface{}
-	if e != nil {
-		e := ResponseError{
-			Data: e.Error(),
-		}
-
-		switch errType {
-		case "notfound":
-			e.Error = "No transcripts"
-			w.WriteHeader(http.StatusNotFound)
-		default:
-			e.Error = "Internal error"
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-
-		s = e
-	} else {
-		type Data struct {
-			Amount      int         `json:"amount"`
-			Transcripts interface{} `json:"transcripts"`
-		}
-		s = ResponseOk{
-			Message: "Ok",
-			Data: Data{
-				Amount:      len(t),
-				Transcripts: t,
-			},
-		}
-	}
-
-	return s
 }
